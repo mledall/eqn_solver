d0969713f51d018e3c12871f165958838a14b5bc
mledall <matthias.ledall08@gmail.com>
2018-04-28 18:36:53 -0700

    Finished the data generating script, and created the script that is going to learn from the data. For now, the learning script only contains the function that imports the data and randomizes it.

--- Data_generator.py
+++ Data_generator.py
@@ -6,7 +6,7 @@ from Boltzmann_eqn import *
 
 def data_gen():
 	epsmin, epsmax, deps = 1e-9, 1e-7, 1e-9
-	Kmin, Kmax, dK = 0, 10, 0.01
+	Kmin, Kmax, dK = 0, 50, 0.01
 	eps = np.arange(epsmin, epsmax, deps)
 	K = np.arange(Kmin, Kmax, dK)
 	N = len(eps) * len(K)
@@ -19,27 +19,18 @@ def data_gen():
 			n = n+1
 			Ylf = final_asymm(eps[i],K[j])
 			if 1.2e-9 > Ylf > 1e-9:
-				v.append([eps[i],K[j],Ylf, 'good'])
+				v.append([eps[i],K[j],Ylf, 'good', 0])
 			else:
-				v.append([eps[i],K[j],Ylf, 'not good'])
+				v.append([eps[i],K[j],Ylf, 'not good', 1])
 			if ( (n+1) % 100 == 0 ):
 				print(' -- Calculated %d points out of %d' % (n,N))
-	asymm = np.array(v)
 	tf = time.time()-t0
-	print('- Finished calculating in %f' %tf)
-	eps = asymm[:,0]
-	K = asymm[:,1]
-	Ylf = asymm[:,2]
-	result = asymm[:,3]
-
-	dfeps = pd.DataFrame(data = eps, columns = ['eps']) #index = ['a','b','c']
-	dfK = pd.DataFrame(data = K, columns = ['K'])
-	dfYlf = pd.DataFrame(data = Ylf, columns = ['Ylf'])
-	dresult = pd.DataFrame(data = result, columns = ['result'])
-	data =  pd.concat([dfeps, dfK, dfYlf, dresult], axis = 1)
+	print('- Finished calculating in %f seconds' %tf)
+	data = pd.DataFrame(data = v, columns = ['eps', 'K', 'Ylf', 'result', 'binary result'])
 	print '- Writing to Asymm_data.csv'
 	data.to_csv('Asymm_data.csv', sep = ',')
 	print '- Finished writing'
+
 
 data_gen()
 
